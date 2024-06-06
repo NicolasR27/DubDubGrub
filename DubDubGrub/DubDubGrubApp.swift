ac05c097783fe87d6e31eb6bf4cc5cea305d7453
Nic Rios <nickrios33@yahoo.com>
2024-06-05 22:09:36 -0700

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Nicolas Rios on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
